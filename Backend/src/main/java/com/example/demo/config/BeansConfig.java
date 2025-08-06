0ba3eb5dcd6d043a1057f9a455bd67f86938b4cd
Wakil.Ahammad <wakil.ahammad@bracits.com>
2025-08-06 21:12:17 +0600

    Configure nginx and docker to containerization

--- Backend/src/main/java/com/example/demo/config/BeansConfig.java
+++ Backend/src/main/java/com/example/demo/config/BeansConfig.java
@@ -54,7 +54,7 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:8080"));
         config.setAllowedHeaders(Arrays.asList(ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION));
 
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
